32258ea5ac141800f7acbe4ba1b98c83bffd2e2b
susheelkumar <susheel4me@gmail.com>
2023-08-20 23:32:08 +0530

    initial commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import {} from "../assets/imgs/banana.jpg";
+const About = () => {
+  return <Container>About</Container>;
+};
+
+export default About;
